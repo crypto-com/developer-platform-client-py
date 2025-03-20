a21b113d9f7f8e0429e2d6e66af1dd5adb810e43
Lim Yee Han <yeehan.lim@crypto.com>
2025-03-20 16:20:31 +0800

    Add startBlock and endBlock arguments

--- crypto_com_developer_platform_client/integrations/transaction_api.py
+++ crypto_com_developer_platform_client/integrations/transaction_api.py
@@ -4,19 +4,21 @@ from ..constants import API_URL
 from .api_interfaces import ApiResponse
 
 
-def get_transactions_by_address(chain_id: str, address: str, session: str, limit: str, api_key: str) -> ApiResponse:
+def get_transactions_by_address(chain_id: str, address: str, startBlock: int, endBlock: int, session: str, limit: str, api_key: str) -> ApiResponse:
     """
     Get transactions by address.
 
     :param chain_id: The ID of the blockchain network
     :param address: The address to get transactions for (CronosIds with the `.cro` suffix are supported, e.g. `xyz.cro`)
-    :param session: The session to get transactions for.
-    :param limit: The limit of transactions to get.
-    :param api_key: The API key for authentication.
-    :return: The transactions for the address.
+    :param startBlock: The starting block number to get transactions from. (The maximum number of blocks that can be fetched is 10,000)
+    :param endBlock: The ending block number to get transactions to. (The maximum number of blocks that can be fetched is 10,000)
+    :param session: The session to get transactions for
+    :param limit: The limit of transactions to get
+    :param api_key: The API key for authentication
+    :return: The transactions for the address
     :rtype: ApiResponse
     """
-    url = f"{API_URL}/transaction/{chain_id}/address?address={address}&session={session}&limit={limit}&apiKey={api_key}"
+    url = f"{API_URL}/transaction/{chain_id}/address?address={address}&startBlock={startBlock}&endBlock={endBlock}&session={session}&limit={limit}&apiKey={api_key}"
 
     response = requests.get(url, headers={'Content-Type': 'application/json'}, timeout=15)
 
--- crypto_com_developer_platform_client/transaction.py
+++ crypto_com_developer_platform_client/transaction.py
@@ -24,16 +24,20 @@ class Transaction:
         cls._client = client
 
     @classmethod
-    def get_transactions_by_address(cls, address: str, session: str = "", limit: str = "20") -> ApiResponse:
+    def get_transactions_by_address(cls, address: str, startBlock: int, endBlock: int, session: str = "", limit: str = "20") -> ApiResponse:
         """
         Get transactions by address.
 
         :param address: The address to get transactions for (CronosIds with the `.cro` suffix are supported, e.g. `xyz.cro`)
+        :param startBlock: The starting block number to get transactions from. (The maximum number of blocks that can be fetched is 10,000)
+        :param endBlock: The ending block number to get transactions to. (The maximum number of blocks that can be fetched is 10,000)
+        :param session: The session to get transactions for
+        :param limit: The limit of transactions to get
         :return: The transactions for the address.
         """
         chain_id = cls._client.get_chain_id()
         api_key = cls._client.get_api_key()
-        return get_transactions_by_address(chain_id, address, session, limit, api_key)
+        return get_transactions_by_address(chain_id, address, startBlock, endBlock, session, limit, api_key)
 
     @classmethod
     def get_transaction_by_hash(cls, hash: str) -> ApiResponse:
